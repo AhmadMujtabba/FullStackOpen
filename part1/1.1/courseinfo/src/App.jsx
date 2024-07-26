edd64491a4d2c852a811706348bae91e7d39fc5c
Ahmad Mujtabba <105150753+AhmadMujtabba@users.noreply.github.com>
2024-07-26 11:10:39 +0500

    part1.2

--- part1/1.1/courseinfo/src/App.jsx
+++ part1/1.1/courseinfo/src/App.jsx
@@ -1,26 +1,37 @@
 import { useState } from 'react'
 //header
-const Header=(props)=>{
-  return(
+const Header = (props) => {
+  return (
     <h1>{props.name}</h1>
   )
 }
 //Content
-  const Content=(props)=>{
-    return(
-      <p>{props.part}{props.number}</p>
-    )
-  }
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.p1} exer={props.e1} />
+      <Part part={props.p2} exer={props.e2} />
+      <Part part={props.p3} exer={props.e3} />
+    </>
+  )
+}
 //Total
-  const Total=(props)=>{
-    const a=props.ex1
-    const b=props.ex2
-    const c=props.ex3
-    return(
-      <p>Number of exercises {a+b+c}</p>
-    )
-  }
-const App=()=>{
+const Total = (props) => {
+  const a = props.ex1
+  const b = props.ex2
+  const c = props.ex3
+  return (
+    <p>Number of exercises {a + b + c}</p>
+  )
+}
+//Part1.2
+//Part
+const Part = (props) => {
+  return (
+    <p>{props.part}{props.exer}</p>
+  )
+}
+const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
@@ -28,14 +39,12 @@ const App=()=>{
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
-  return(
+  return (
     <div>
-    <Header name={course}/>
-    <Content part={part1} number={exercises1}/>
-    <Content part={part2} number={exercises2}/>
-    <Content part={part3} number={exercises3}/>
-    <Total ex1={exercises1} ex2={exercises2} ex3={exercises3}/>
-  </div>
+      <Header name={course} />
+      <Content p1={part1} e1={exercises1} p2={part2} e2={exercises2} p3={part3} e3={exercises3} />
+      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
+    </div>
   )
 }
 export default App
