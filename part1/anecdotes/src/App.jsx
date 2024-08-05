d7cf22fbb7cfe958025beb68af4080b3ce95455d
Ahmad Mujtabba <105150753+AhmadMujtabba@users.noreply.github.com>
2024-08-05 23:28:47 +0500

    part 1.13

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 
 function App() {
   const [selected, setSelected] = useState(0)
-  const quotes = [
+  const [point,setPoint]=useState([0,0,0,0,0])
+  const quotes = 
+  [
     'Adding manpower to a late software project makes it later!',
     'The best way to get a project done faster is to start sooner-Jim Highsmith',
     'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.-Tom Cargill',
@@ -12,7 +14,12 @@ function App() {
   const randomnumgen = () => {
     let num = Math.floor(Math.random() * quotes.length)
     setSelected(num)
-    return (console.log(num))
+  }
+
+  const votecounter=()=>{
+    const vote=[...point]
+    vote[selected]+=1
+    setPoint(vote)
   }
   return (
     <div>
@@ -20,6 +27,15 @@ function App() {
       <button onClick={randomnumgen}>
         Next Anecdote
       </button>
+      <button onClick={votecounter}>
+        Vote
+      </button>
+      <p>0 : {point[0]}</p>
+      <p>1 : {point[1]}</p>
+      <p>2 : {point[2]}</p>
+      <p>3 : {point[3]}</p>
+      <p>4 : {point[4]}</p>
+
     </div>
   )
 }
