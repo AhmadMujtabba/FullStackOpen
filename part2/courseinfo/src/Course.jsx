3ea97f6b4c39a473ba26541f03cb04981c11fb2a
Ahmad Mujtabba <105150753+AhmadMujtabba@users.noreply.github.com>
2024-08-08 19:12:50 +0500

    part 2.2

--- part2/courseinfo/src/Course.jsx
+++ part2/courseinfo/src/Course.jsx
@@ -1,10 +1,13 @@
 import Header from "./Header";
 import Content from "./Content";
+import Total from "./Total";
 const Course = ({course}) => {
     return (
         <div>
             <Header name={course.name} />
             <Content partsref={course.parts} />
+            <Total sum={course.parts}/>
+
         </div>
     )
 }
--- part2/courseinfo/src/Total.jsx
+++ part2/courseinfo/src/Total.jsx
@@ -1,2 +1,13 @@
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+const Total = ({ sum }) => {
+const arr=sum.map(n=>n.exercises)
+ let s=0
+ for(let i=0;i<arr.length;i++)
+ {
+    s=arr[i]+s
+ }
+ return(
+    <b><p>Total exercises {s}</p></b>
+ )
+}
+
 export default Total
